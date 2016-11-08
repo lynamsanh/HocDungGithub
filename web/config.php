af32dabf3372e08b113a2178c95607fb8370a9e7
lynamsanh <sanhc1500408@gmail.com>
2016-11-08 10:33:32 +0700

    ok

--- web/config.php
+++
@@ -1,5 +0,0 @@
-<?php
- $conn=mysql_connect("localhost","root","") or die("can't connect this database");
- mysql_select_db("khachsan",$conn);
-mysql_query("set names 'utf8'");
-?>
--- web/dkphong/nhan.php
+++
@@ -1,8 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php 
-include('../config.php'); 
-$tg=date("Y-m-d");
-$sql3="UPDATE `phieudk` SET 	`NGAYDEN`= '".$tg."' where  phieudk.MaPhong=  '".$_GET['mp']."' ";
-$query3=mysql_query($sql3);
-header("location:thontin.php?mp=".$_GET['mp']."");
-?>
